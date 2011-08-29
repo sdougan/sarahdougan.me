c0be24ce4c03b61aec7a9aacf1ecdc49db0be6a5
Sarah Dougan <sarahdougan@me.com>
2011-08-28 19:54:54 -0700

    Updating portfolio

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,20 @@ class HomeController < ApplicationController
   def index
     
   end
+  
+  def blog
+    @active = "blog"
+  end
+  
+  def contact
+    @active = "contact"
+  end
+  
+  def about
+    @active = "about"
+  end
+  
+  def portfolio
+    @active = "portfolio"
+  end
 end
