31f7f4ed114a349a0cee5e7c241c62b7efe2e755
Sarah Dougan <sarahdougan@me.com>
2013-11-27 00:55:18 +0100

    website update

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ SarahdouganMe::Application.routes.draw do
 
     match 'posts/:permalink' => 'posts#show', :as => :post
 
-    root :to => "home#portfolio_dribbble"
+    root :to => "home#about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
