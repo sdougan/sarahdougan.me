de453de9cf155c72629d0aefd408aac631a21730
Sarah Dougan <sarahdougan@me.com>
2014-01-26 16:12:46 +0100

    adding field type

--- db/migrate/20140124215825_create_works.rb
+++ db/migrate/20140124215825_create_works.rb
@@ -2,13 +2,9 @@ class CreateWorks < ActiveRecord::Migration
   def change
     create_table :works do |t|
       t.string :title
-
       t.string :description
-
       t.string :category
-
       t.string :date
-
       t.string :image
       
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140126150623) do
+ActiveRecord::Schema.define(:version => 20140126151054) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(:version => 20140126150623) do
   end
 
   create_table "works", :force => true do |t|
-    t.string   "title"
+    t.text     "title",       :limit => 255
     t.text     "description", :limit => 255
-    t.string   "category"
-    t.string   "date"
-    t.string   "image"
+    t.text     "category",    :limit => 255
+    t.text     "date",        :limit => 255
+    t.text     "image",       :limit => 255
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
   end
